b57705f4436b9ffa95e632cf3d98a3b6950ac1ee
BartekJunior <bartekprzybysz@gmail.com>
2023-12-11 14:40:05 +0100

    pubNub works

--- pubnub.js
+++ pubnub.js
@@ -1,6 +1,7 @@
-const UUID = `bartek`;
 
-const sendButton = document.getElementById(`sendButton`);
+// const UUID = `bartek`;
+const UUID = prompt(`Write Player's Name`);
+
 
 document.addEventListener(`keydown`, function (event) {
   if (event.key === `Enter`) buttonClick();
@@ -14,6 +15,7 @@ const buttonClick = () => {
 
 const showMessage = (messageEvent) => {
   const msgContainer = document.createElement("div");
+  msgContainer.classList.add(`msg-container`);
 
   const publisher = document.createElement("div");
   publisher.classList.add(`chat-publisher`, `chat-msg`);
@@ -43,19 +45,26 @@ const setupPubNub = () => {
     status: (statusEvent) => {
       if (statusEvent.category === "PNConnectedCategory") {
         console.log("Connected");
+        // handleConnected();
       }
     },
 
     message: (messageEvent) => {
       // showMessage(messageEvent.message.description);
       showMessage(messageEvent);
-
-      console.log(messageEvent);
+      console.log(`MESSAGE EVENT`, messageEvent);
     },
 
-    presence: (presenceEvent) => {
-      // handle presence
+    presence: (event) => {
+      console.log(`PRESENCE EVENT`, event);
+      
+      if (event.action === "join") {
+        console.log(`User ${UUID} has joined.`);
+      } else if (event.action === "leave") {
+        console.log(`User ${UUID} has left.`);
+      }
     },
+
   };
 
   pubnub.addListener(listener);
@@ -63,11 +72,28 @@ const setupPubNub = () => {
   // subscribe to a channel
   pubnub.subscribe({
     channels: ["hello_world"],
+    withPresence: true,
   });
 };
 
 // run after page is loaded
+
 window.onload = setupPubNub;
+
+// window.onload = () => {
+//   console.log(`before unsubscrube`);
+  
+//   if (pubnub) {
+//     pubnub.unsubscribe({
+//       channels: ["hello_world"],
+//     });
+//   }
+//   console.log(`After unsubscribe`);
+  
+//   setupPubNub();
+// };
+
+
 
 // publish message
 const publishMessage = async (message) => {
